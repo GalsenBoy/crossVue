d51455b0be1ea7a66ffa98f7e1eb1266e97a806e
bilaly <billycsk@gmail.com>
2023-02-01 14:51:31 +0100

    Vue init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <ProductComponent />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductComponent from './components/ProductComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ProductComponent
   }
 }
 </script>
---
+++ src/components/ProductComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Liste des produits :</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
